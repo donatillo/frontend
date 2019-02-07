89f8f4f7e76af44b4f8dec05f4918572f523a015
André Wagner <andre.nho@gmail.com>
2019-02-06 22:30:49 -0200

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,4 +34,54 @@ resource "aws_s3_bucket" "frontend" {
     }
 }
 
+#
+# CLOUDFRONT
+#
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "frontend_cf" {
+    origin {
+        domain_name         = "give-and-take-${var.env}.s3.amazonaws.com"
+        origin_id           = "give-and-take-${var.env}-s3"
+    }
+
+    enabled                 = true
+    is_ipv6_enabled         = true
+    default_root_object     = "index.html"
+
+    aliases = ["${var.env}.give-and-take.tk"]
+
+    default_cache_behaviour {
+        allowed_methods     = ["GET", "HEAD", "OPTIONS"]
+        cached_methods      = ["GET", "HEAD", "OPTIONS"]
+        target_origin_id    = "give-and-take-${var.env}-s3"
+
+        forwarded_values {
+            query_string    = false
+
+            cookies {
+                forward     = "none"
+            }
+        }
+
+        viewer_protocol_policy = "allow-all"
+        min_ttl                = 0
+        default_ttl            = 86400
+        max_ttl                = 31636000
+
+        compress               = true
+        viewer_protocol_policy = "redirect-to-https"
+    }
+
+    price_class                 = "PriceClass_200"
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+
+}
+
 # vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
