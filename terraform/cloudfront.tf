23c13e41d6606fad4377cedb57a4ff8756544737
André Wagner <andre.nho@gmail.com>
2019-02-08 11:32:22 -0200

    .

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -40,12 +40,13 @@ resource "aws_cloudfront_distribution" "frontend_cf" {
 
     restrictions {
         geo_restriction {
-            restriction_type = "none"
+            restriction_type    = "none"
         }
     }
 
     viewer_certificate {
-        acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+        acm_certificate_arn     = "${aws_acm_certificate.cert.arn}"
+        ssl_support_method      = "sni-only"
     }
 
 }
---
+++ terraform/dns.tf
@@ -0,0 +1,32 @@
+data "aws_route53_zone" "zone" {
+    name                = "${var.domain}"
+    private_zone        = false
+}
+
+// IPv4
+resource "aws_route53_record" "jenkins-dns" {
+    zone_id         = "${aws_route53_zone.primary.zone_id}"
+    name            = "${var.env}.${var.domain}"
+    type            = "A"
+    ttl             = "300"
+    alias {
+        name        = "${aws_cloudfront_distribution.frontend_cf.domain_name}"
+        zone_id     = "${aws_cloudfront_distribution.frontend_cf.hosted_zone_id}"
+        evaluate_target_health = true
+    }
+}
+
+// IPv6
+resource "aws_route53_record" "jenkins-dns" {
+    zone_id         = "${aws_route53_zone.primary.zone_id}"
+    name            = "${var.env}.${var.domain}"
+    type            = "AAAA"
+    ttl             = "300"
+    alias {
+        name        = "${aws_cloudfront_distribution.frontend_cf.domain_name}"
+        zone_id     = "${aws_cloudfront_distribution.frontend_cf.hosted_zone_id}"
+        evaluate_target_health = true
+    }
+}
+
+# vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
