cd6f1e7f35a0c4e51cc3b9a7ce60796dd12026c3
Andre Wagner <andre.nho@gmail.com>
2019-02-24 09:22:29 -0300

    .

--- src/Login.js
+++ src/Login.js
@@ -7,12 +7,12 @@ const responseGoogle = (response) => {
   const id_token = response.getAuthResponse().id_token;
 
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', process.env.REACT_APP_BACKEND_URL + '/tokensignin');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.open('POST', process.env.REACT_APP_BACKEND_URL + '/tokensignin', true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
   xhr.onload = function() {
       console.log('Signed in as: ' + xhr.responseText);
   };
-  xhr.send('idtoken=' + id_token);
+  xhr.send(JSON.stringify({ "token": id_token }));
 }
 
 class Login extends Component {
