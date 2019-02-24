0d805ba6b77144b293d218fdd8b6e2aa5eef531e
Andre Wagner <andre.nho@gmail.com>
2019-02-23 22:37:01 -0300

    .

--- src/Login.js
+++ src/Login.js
@@ -4,17 +4,15 @@ import GoogleLogin from 'react-google-login';
 
 const responseGoogle = (response) => {
   console.log(response);
-  console.log(response.getAuthResponse().id_token);
+  const id_token = response.getAuthResponse().id_token;
 
-  /*
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', process.env.REACT_APP_BACKEND_URL + 'tokensignin');
+  xhr.open('POST', process.env.REACT_APP_BACKEND_URL + '/tokensignin');
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.onload = function() {
       console.log('Signed in as: ' + xhr.responseText);
   };
   xhr.send('idtoken=' + id_token);
-  */
 }
 
 class Login extends Component {
